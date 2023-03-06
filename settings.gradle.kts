aa42846a45b0a8e451626d7874d1f078deb4d5c4
kisonar <kisonar@wp.pl>
2023-03-06 21:43:28 +0100

    Plugins Management

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,11 +9,5 @@ pluginManagement {
         id("org.jetbrains.kotlin.plugin.spring") version "1.6.21"
         id("org.springframework.boot") version "2.7.4"
         id("io.spring.dependency-management") version "1.0.13.RELEASE"
-
-    }
-    resolutionStrategy {
-    }
-    repositories {
-        //  mavenCentral()
     }
 }
