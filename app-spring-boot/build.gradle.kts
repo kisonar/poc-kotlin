6d57c19162f52eb933030bfc7438ffd5ae0c3d66
kisonar <kisonar@wp.pl>
2023-03-06 21:22:50 +0100

    Adding GHA

--- app-spring-boot/build.gradle.kts
+++ app-spring-boot/build.gradle.kts
@@ -7,11 +7,19 @@ plugins {
 }
 
 dependencies {
+    // internal
+    implementation(project(":service"))
+    // Spring
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
-
-    implementation(project(":service"))
+    // test
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude("org.mockito")
+    }
+    testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("io.mockk:mockk:1.13.2")
+    testImplementation("com.ninja-squad:springmockk:3.1.1")
 }
 
 springBoot {
