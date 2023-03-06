38dc20c4a0b49c44f54b0eee94d1fabb3e763c76
kisonar <kisonar@wp.pl>
2023-03-06 21:02:42 +0100

    Multi node project

---
+++ app-spring-boot/build.gradle.kts
@@ -0,0 +1,22 @@
+
+plugins {
+    id("org.springframework.boot") version "2.7.4"
+    id("io.spring.dependency-management") version "1.0.13.RELEASE"
+    id("org.jetbrains.kotlin.jvm") version "1.6.21"
+    id("org.jetbrains.kotlin.plugin.spring") version "1.6.21"
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+}
+
+springBoot {
+    buildInfo()
+    mainClass.set("com.fortum.hiven.spot.prices.ApplicationKt")
+}
+
+tasks.jar {
+    enabled = false
+}
