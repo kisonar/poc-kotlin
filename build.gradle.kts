95d1978c962d2faa6e4896880e8334ceb25a6792
marcin <marcin.migdal@fortum.com>
2025-08-18 14:51:23 +0200

    update libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,11 +6,4 @@ allprojects {
     repositories {
         mavenCentral()
     }
-
-    //dependencies {
-    //    testImplementation("org.junit.jupiter:junit-jupiter-engine:6.0.2")
-    //    testRuntimeOnly("org.junit.platform:junit-platform-launcher:6.0.2")
-    //}
 }
-
-
