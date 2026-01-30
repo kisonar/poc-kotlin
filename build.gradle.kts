5c44029fbf0834b61eff9c026eeb25cbb8222d22
Marcin Migdal <marcinmigdal@wp.pl>
2026-01-30 15:58:55 +0100

    fix build 6

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,10 +7,10 @@ allprojects {
         mavenCentral()
     }
 
-    dependencies {
-        testImplementation("org.junit.jupiter:junit-jupiter-engine:6.0.2")
-        testRuntimeOnly("org.junit.platform:junit-platform-launcher:6.0.2")
-    }
+    //dependencies {
+    //    testImplementation("org.junit.jupiter:junit-jupiter-engine:6.0.2")
+    //    testRuntimeOnly("org.junit.platform:junit-platform-launcher:6.0.2")
+    //}
 }
 
 
