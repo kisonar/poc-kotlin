0f2454f27a06bf1354435b746755167996227cea
Marcin Migdal <marcinmigdal@wp.pl>
2026-01-30 15:52:00 +0100

    fix build 5

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,13 +6,10 @@ allprojects {
     repositories {
         mavenCentral()
     }
-    // kotlin {
-    //    jvmToolchain(17)
-    //}
+
     dependencies {
-        /* all projects have JUnit + Mockito activated in compilation & execution*/
-        testImplementation ("org.junit.jupiter:junit-jupiter-engine:6.0.2")
-        testRuntimeOnly ("org.junit.platform:junit-platform-launcher:6.0.2")
+        testImplementation("org.junit.jupiter:junit-jupiter-engine:6.0.2")
+        testRuntimeOnly("org.junit.platform:junit-platform-launcher:6.0.2")
     }
 }
 
