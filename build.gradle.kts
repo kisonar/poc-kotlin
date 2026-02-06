4e5e007fcc967b74346eb6ddfd4ac8c7b707cb01
marcin <marcin.migdal@fortum.com>
2026-02-06 13:41:39 +0100

    Update libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    `java-library`
+    id("org.jetbrains.kotlin.jvm")
 }
+
 
 allprojects {
 
@@ -16,8 +17,9 @@ allprojects {
         }
     }
 
+    // TODO: https://docs.gradle.org/current/userguide/sharing_build_logic_between_subprojects.html
+
+
+
+
 }
-
-
-
-
