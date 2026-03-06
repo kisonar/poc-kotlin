b45d6e14520428802df6b2c11334b018d530b7c3
marcin <marcin.migdal@fortum.com>
2026-03-06 09:25:24 +0100

    cleanup

--- service/src/main/kotlin/kisonar/poc/kotlin/service/middleware/MiddlewareService.kt
+++ service/src/main/kotlin/kisonar/poc/kotlin/service/middleware/MiddlewareService.kt
@@ -5,7 +5,9 @@ import kisonar.poc.kotlin.library.user.UserFactory
 
 class MiddlewareService {
 
-    fun fetchUsers() : List<User> = UserFactory.getUsers()
-    fun checkList() {
+    fun fetchUsers(): List<User> = UserFactory.getUsers()
+    suspend fun checkList(traceId: String, block: suspend () -> Unit) {
+        println(traceId)
+        block()
     }
 }
